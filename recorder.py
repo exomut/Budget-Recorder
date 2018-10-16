92117196b14081ee42a38d29a1dfe4978ebb842a
Seth Van Roekel <exomut@gmail.com>
2018-10-16 23:34:19 +0900

    Added Category selection checking and cleaned formatting.

--- recorder.py
+++ recorder.py
@@ -34,24 +34,29 @@ def button_pressed(button):
 		price_amount.text = '0'
 		
 	if title == 'write':
+		if len(category.selected_rows) == 0:
+			alert("Please select a category")
+			return
+		
 		category_name = category.data_source.items[category.selected_row[1]]
 
 		if agent_name.text == '':
-			alert('Please enter an agent name.')
+			alert("Please enter an agent name.")
 			return
 		
 		date_format = '%Y-%m-%d'
-		formatted_data = \
-			f'{transfer_date.date:{date_format}},' \
-			f'{agent_name.text},' \
-			f'{price_amount.text},' \
-			f'{category_name}\n'
+		formatted_data = (
+			f"{transfer_date.date:{date_format}},"
+			f"{agent_name.text},"
+			f"{price_amount.text},"
+			f"{category_name}\n"
+			)
 					
 		with io.open(DATA_FILE, 'a+', encoding='utf8') as f:
 			f.write(formatted_data)
-			logger.info(f'"{formatted_data[:-1]}" appended to "{DATA_FILE}" file.')
+			logger.info(f"\"{formatted_data[:-1]}\" appended to \"{DATA_FILE}\" file.")
 		reload_records()
-		hud_alert(f'Added entry for {agent_name.text}.')
+		hud_alert(f"Added entry for {agent_name.text}.")
 
 
 def reload_records():
@@ -62,7 +67,7 @@ def reload_records():
 			records.data_source = entries
 			records.reload_data()
 	except FileNotFoundError:
-		logger.info(f'"{DATA_FILE}" does not exist yet, so data did not load.')
+		logger.info(f"\"{DATA_FILE}\" does not exist yet, so data did not load.")
 
 
 if __name__ == '__main__':
